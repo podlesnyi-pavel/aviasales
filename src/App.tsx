70f6c3eea34d5764c6981cbb4e04d8fb36585c7a
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2025-02-21 18:40:02 +0300

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div>
+      <input type="text" style={{ border: '1px solid red' }} />
+    </div>
+  );
+};
+
+export default App;
